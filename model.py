db33cf45b9c6311c225041a252dc8dc62cac7cb3
Anwar Khamis <119974246+AnwarHawash1@users.noreply.github.com>
2024-05-09 04:56:57 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
-from transformers import ViTForImageClassification, ViTFeatureExtractor
+from transformers import ViTForImageClassification, ViTImageProcessor
 import torch.nn as nn
 
 class CustomViTModel(nn.Module):
     def __init__(self, num_classes):
         super(CustomViTModel, self).__init__()
         self.vit = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224-in21k")
-        self.feature_extractor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.feature_extractor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
         self.num_classes = num_classes
         self.classifier = nn.Linear(self.vit.config.hidden_size, self.num_classes)
 
